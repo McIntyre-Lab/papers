3460e0a6467fc792769d767cd590d67e78fe310d
avnanni <60658947+avnanni@users.noreply.github.com>
2020-02-19 17:21:47 -0500

    Add modified tofu2 script

---
+++ morse_maize_2020/scripts/isoseq3_make_cluster_report_cup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import os, sys, re
+from csv import DictWriter
+import pysam
+
+report_fields = ['cluster_id', 'read_id', 'read_type']
+
+
+def make_cluster_report_from_polished_bam(polished_bam,output_prefix):
+    f = pysam.Samfile(polished_bam, check_sq=False)
+    h = open(output_prefix+'.cluster_report.csv', 'w')
+    writer = DictWriter(h, fieldnames=report_fields, delimiter=',')
+    writer.writeheader()
+    for r in f:
+        d = dict(r.tags)
+        # transcript name format: transcript/0
+        # zmw is of format <movie>/<zmw>/ccs, which is the ID for the FL read
+        for zmw in d['im'].split(','):
+            h.write("{0},{1},FL\n".format(r.qname, zmw))
+
+    h.close()
+    print >> sys.stderr, "Cluster report  written to: {0}".format(h.name)
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser("Make cluster_report.csv for Iso-Seq3 cluster output.")
+    parser.add_argument("polished_bam", help="Polished bam file")
+    parser.add_argument("output_prefix", help="Output prefix for cluster_report.csv")
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.polished_bam):
+        print >> sys.stderr, "{0} does not exist. Abort!".format(args.polished_bam)
+        sys.exit(-1)
+
+    make_cluster_report_from_polished_bam(args.polished_bam,args.output_prefix)
