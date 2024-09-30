bd5e90f6d12d829158b44eea23f74e0048175b55
Kinfeosioluwa Bankole <k.bankole@ufgi-grimshawi.ahc.ufl.edu>
2024-09-30 14:27:01 -0400

    created spyder project for ease of editing. working on creating a 'fourgene' plot for all genomes. 'fourgene' plot is now five genes

--- bankole_sex_specific_splicing_2024/scripts/create_manuscript_upset_plots_01ksb.py
+++ bankole_sex_specific_splicing_2024/scripts/create_manuscript_upset_plots_01ksb.py
@@ -2,6 +2,7 @@
 
 import sss_upset_plot_functions_01ksb as plotter
 import glob
+import pandas as pd
 
 
 def main():
@@ -15,23 +16,33 @@ def main():
     for flagFile in mergeFlagFiles:
         plotter.plotSharedJxnHashPerGenome(flagFile=flagFile, outdir=outdir)
 
-    # Create jxnHash upset plot for below 4 genes on *MEL*
-    # 4 Gene == Fru (FBgn0004652), Dsx (FBgn0000504), Sxl (FBgn0264270), Doa (FBgn0264270)
-    fourGeneDct = {
+    # Create jxnHash upset plot for below 4 genes on *ALL GENOME*
+    # 5 Gene == Fru (FBgn0004652), Dsx (FBgn0000504), Sxl (FBgn0264270), Doa (FBgn0264270), Tra2 (FBgn0003742)
+    fiveGeneDct = {
         "Doa": "FBgn0265998",
         "Dsx": "FBgn0000504",
         "Fru": "FBgn0004652",
-        "Sxl": "FBgn0264270"
+        "Sxl": "FBgn0264270",
+        "Tra2": "FBgn0003742"
     }
-    genome = "dmel6"
 
-    plotter.plotGeneJxnHash(
-        f"{indir}/fiveSpecies_2_dmel6_ujc_gene_key.csv",
-        f"{indir}/flag_fiveSpecies_2_dmel6_ujc.csv",
-        fourGeneDct,
-        genome,
-        outdir
-    )
+    for genome, otherName in {"dmel6": None, "dsim2": 'dsim', "dsan1": 'dsan', "dyak2": 'dyak', "dser1": 'dser'}.items():
+
+        if genome == 'dmel6':
+            geneDct = fiveGeneDct
+        else:
+            speciesLnkFile = f"{indir}/{otherName}_sexdet.csv"
+            speciesLnkDf = pd.read_csv(speciesLnkFile, low_memory=False)
+            speciesLnkDf = speciesLnkDf[[
+                col for col in speciesLnkDf.columns if 'geneID' in col]]
+
+        plotter.plotGeneJxnHash(
+            f"{indir}/fiveSpecies_2_{genome}_ujc_gene_key.csv",
+            f"{indir}/flag_fiveSpecies_2_{genome}_ujc.csv",
+            geneDct,
+            genome,
+            outdir
+        )
 
     for genome in ["dmel6", "dsim2", "dsan1", "dyak2", "dser1"]:
         flagFile = ("/nfshome/k.bankole/mclab/SHARE/McIntyre_Lab/"
